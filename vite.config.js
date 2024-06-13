b9323d8e72371a329c730ce5125e7ecd1f191802
Bryan974p <bryan.francomme@gmail.com>
2024-06-13 22:43:53 +0400

    Configure base path for GitHub Pages

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/site_cf_saint_denis/', // Assurez-vous que ce chemin correspond au nom de votre dépôt GitHub
+})

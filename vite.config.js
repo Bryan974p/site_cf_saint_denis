2a30d8e81c95cdf207ba938c44191501c79dfbfa
Bryan974p <bryan.francomme@gmail.com>
2024-06-13 23:37:44 +0400

    Fix hamburger menu and ensure mobile responsiveness

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-
-// vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/site_cf_saint_denis/', // Assurez-vous que ce chemin correspond au nom de votre dépôt GitHub
-})
+  base: '/site_cf_saint_denis/', // Assurez-vous que cette ligne est correcte
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+});
